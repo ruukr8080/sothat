a2f0767ef3458c3085b82eeaaa6f3ee34687a0e9
han <131014787+ruukr8080@users.noreply.github.com>
2024-09-21 22:12:03 +0900

    first commit

---
+++ src/main/java/com/ex/sothat/SothatApplication.java
@@ -0,0 +1,13 @@
+package com.ex.sothat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SothatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SothatApplication.class, args);
+	}
+
+}
